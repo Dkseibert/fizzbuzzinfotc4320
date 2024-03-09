93aca1a81309164a4d1eadd5fbf98c66f2b3bd14
Dkseibert <89469804+Dkseibert@users.noreply.github.com>
2024-03-08 19:10:23 -0600

    Add files via upload

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,2 +1,9 @@
 for fizzbuzz in range (101):
     print(fizzbuzz)
+
+    if fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
